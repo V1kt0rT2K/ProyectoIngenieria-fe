d1b7496f42b56326bc14e2ecfff1bf2c17a014de
Viktor Hernandez <victorandrehernandezvelasquez@gmail.com>
2025-07-01 21:56:42 -0600

    adding allowedHost for remote deploying

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 9000,
+    },
+    server:{
+        allowedHosts: ['proyectoingenieria-fe.onrender.com']
     }
 })

afe4ac0576ad6deb579a9d84c2bd820c6cc25257
Viktor Hernandez <victorandrehernandezvelasquez@gmail.com>
2025-07-07 18:21:30 -0600

    adding searchUser service

--- src/utils/service/UserService.js
+++ src/utils/service/UserService.js
@@ -17,6 +17,21 @@ class UserService {
 
         return await result.json();
     }
+
+    static async searchUsers(searchParam) {
+        
+        const result = await fetch(`
+            ${Configuration.API_BASE_URL}/users/search/${searchParam}`, 
+        {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        });
+
+        return await result.json();
+    }
 }
 
 export default UserService;
